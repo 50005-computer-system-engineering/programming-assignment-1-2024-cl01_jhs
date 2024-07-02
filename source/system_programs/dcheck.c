cf67f67b00bc8b604080cd6ec3b2b06fcbea1bc6
janyaa07 <140350660+janyaa07@users.noreply.github.com>
2024-07-03 05:49:16 +0800

    Update dcheck.c

--- source/system_programs/dcheck.c
+++ source/system_programs/dcheck.c
@@ -9,12 +9,13 @@ int main()
 {
     char buffer[128];
     int count = 0;
-    FILE *fp;
+    FILE *fp; // file pointer 
 
     // Open the command for reading
     fp = popen(COMMAND, "r");
     if (fp == NULL)
     {
+        //error message if command fails
         fprintf(stderr, "Failed to run command\n");
         exit(EXIT_FAILURE);
     }
@@ -28,6 +29,7 @@ int main()
     // Close the command stream
     pclose(fp);
 
+    // Print the number of 'dspawn' processes found
     printf("Number of live dspawn daemon processes: %d\n", count);
 
     return EXIT_SUCCESS;
